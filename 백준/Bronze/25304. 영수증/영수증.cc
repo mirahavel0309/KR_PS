b1e1f9f22c5c467d1ba9d01df4c3f603fb2e24ab
MiraHavel <bagjinhyeong640@gmail.com>
2025-12-26 07:32:03 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, n;
+    int sum = 0;
+    cin >> x >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        sum += a * b;
+    }
+    cout << (sum == x ? "Yes" : "No");
+}
