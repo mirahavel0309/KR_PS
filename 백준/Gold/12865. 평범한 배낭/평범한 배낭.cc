9ef99f8b6cc8a342367b1491e996ae718028a886
MiraHavel <bagjinhyeong640@gmail.com>
2026-04-26 22:17:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2476 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k;cin>>n>>k;
+    vector<int> dp(k+1,0);
+    for(int i=0;i<n;i++){
+        int w,v;cin>>w>>v;
+        for(int j=k;j>=w;j--)dp[j]=max(dp[j],dp[j-w]+v);
+    }
+    cout<<dp[k];
+}
