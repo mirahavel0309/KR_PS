4d58c923c28e4e1b2f23e23d90b662f051fe398a
MiraHavel <bagjinhyeong640@gmail.com>
2026-04-26 22:17:35 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 100 ms, Memory: 6188 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int s[1025][1025];
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int n,m;cin>>n>>m;
+    for(int i=1;i<=n;i++)for(int j=1;j<=n;j++){int x;cin>>x;s[i][j]=x+s[i-1][j]+s[i][j-1]-s[i-1][j-1];}
+    while(m--){
+        int x1,y1,x2,y2;cin>>x1>>y1>>x2>>y2;
+        cout<<s[x2][y2]-s[x1-1][y2]-s[x2][y1-1]+s[x1-1][y1-1]<<"\n";
+    }
+}
